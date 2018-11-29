c118ede350706e500b220da76a026c1cae0aa33d
Andrii Kicha <akicha@cogniance.com>
2018-11-29 12:03:50 +0200

    Implement router

--- src/App.js
+++ src/App.js
@@ -1,37 +1,15 @@
 import React from 'react'
-import Grid from '@material-ui/core/Grid'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 
-import MediaCard from './MediaCard/MediaCard.component'
+import HomePage from './Home.component'
 
 import './App.css'
 
 const App = () => (
   <div className="container">
-    <Grid
-      container
-      spacing={40}
-    >
-      <Grid
-        item
-        xs={6}
-      >
-        <MediaCard
-          title="Norsemen"
-          subtitle="by Netflix"
-          cover="https://nypdecider.files.wordpress.com/2017/08/norsemen.png?w=646&h=431&crop=1"
-        />
-      </Grid>
-      <Grid
-        item
-        xs={6}
-      >
-        <MediaCard
-          title="Narcos"
-          subtitle="by Netflix"
-          cover="https://occ-0-987-990.1.nflxso.net/art/eb3bf/f0c7a05129d5611390af4a9f41fbf136bdeeb3bf.jpg"
-        />
-      </Grid>
-    </Grid>
+    <Router>
+      <Route path="/" component={HomePage} />
+    </Router>
   </div>
 )
 
---
+++ src/Home.component.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+import Grid from '@material-ui/core/Grid'
+import MediaCard from './MediaCard/MediaCard.component'
+
+export default () => (
+  <>
+    <h1>Continue watching:</h1>
+    <Grid
+      container
+      spacing={40}
+    >
+      <Grid
+        item
+        xs={6}
+      >
+        <MediaCard
+          title="Norsemen"
+          subtitle="by Netflix"
+          cover="https://nypdecider.files.wordpress.com/2017/08/norsemen.png?w=646&h=431&crop=1"
+        />
+      </Grid>
+      <Grid
+        item
+        xs={6}
+      >
+        <MediaCard
+          title="Narcos"
+          subtitle="by Netflix"
+          cover="https://occ-0-987-990.1.nflxso.net/art/eb3bf/f0c7a05129d5611390af4a9f41fbf136bdeeb3bf.jpg"
+        />
+      </Grid>
+    </Grid>
+  </>
+)
